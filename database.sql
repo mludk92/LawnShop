9a8d592f8317a2be978a32e9e4e719fc0c234b89
Mitch Ludkey <mludk92@hotmail.com>
2023-05-16 17:49:28 -0500

    updated database.sql with create

--- database.sql
+++ database.sql
@@ -3,8 +3,36 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+
+--database lawn_shop
+
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR UNIQUE NOT NULL,
+  password VARCHAR NOT NULL,
+);
+
+CREATE TABLE sales (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  fromdate DATE NOT NULL,
+  todate DATE NOT NULL
+);
+
+CREATE TABLE address (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
+  city VARCHAR NOT NULL,
+  state VARCHAR NOT NULL,
+  street VARCHAR null,
+  zip INTEGER null
+);
+
+CREATE TABLE featured_items (
+  id SERIAL PRIMARY KEY,
+  sales_id INTEGER REFERENCES sales(id),
+  item VARCHAR NOT NULL,
+  price DECIMAL null,
+  desc VARCHAR null
 );

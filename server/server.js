1d1e5602a89cc7b9b13a6b23447c364dc0122c28
Mitch Ludkey <mludk92@hotmail.com>
2023-05-16 19:48:30 -0500

    added .env

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use('/api/featured', featuredRouter)
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5071;
 
 /** Listen * */
 app.listen(PORT, () => {

001694af7a8b59d7acddee55919f41d1c50b5dcd
Mitch Ludkey <mludk92@hotmail.com>
2023-05-28 16:16:02 -0500

    setting up the map

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,30 @@
 import React, { useEffect } from 'react';
+import "./UserPage.css"
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
+import { 
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow
+ } from '@react-google-maps/api'; 
+ import { formatRelative } from 'date-fns';
+
+ import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete'
+ //import { ComboBox, ComboBoxInput, ComboBoxPopover, ComboboxList, ComboBoxOption} from "@reach/combobox/styles.css"
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
   const address = useSelector((store)=> store.address)
-
+  const libraries = ["places"]
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+    libraries,
+  })
+  
   useEffect(() => {
     dispatch({ type: 'FETCH_ADDRESS' });
   }, [dispatch]);
@@ -18,9 +34,12 @@ function UserPage() {
   const [street, setStreet] = useState(null)
   const [zip, setZip] = useState(null)
 
+
+  if (loadError) return "Error loading maps"
+  if (!isLoaded) return "Loading Maps"
   return (
     <div className="container">
-      <h2>basemap goes here </h2>
+      <div className='MapContainer'>basemap goes here </div>
       <div> Test 
       <div>{JSON.stringify(address)}</div> 
       </div>

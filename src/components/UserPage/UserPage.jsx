e7f32ae9c25d339239b894578316be449cfe225f
Mitch Ludkey <mludk92@hotmail.com>
2023-05-28 20:09:25 -0500

    points and centering functions are working

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -8,40 +8,61 @@ import {
   useLoadScript,
   Marker,
   InfoWindow
- } from '@react-google-maps/api'; 
- import { formatRelative } from 'date-fns';
- import mapStyles from './mapStyles';
+} from '@react-google-maps/api'; 
+import { formatRelative } from 'date-fns';
+import mapStyles from './mapStyles';
 
- import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete'
- //import { ComboBox, ComboBoxInput, ComboBoxPopover, ComboboxList, ComboBoxOption} from "@reach/combobox/styles.css"
+import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete'
 
 function UserPage() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
-  const address = useSelector((store)=> store.address)
+  const addresses = useSelector((store)=> store.address)
   const libraries = ["places"]
-  const {isLoaded, loadError} = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
     libraries,
-  })
+  });
   
   useEffect(() => {
     dispatch({ type: 'FETCH_ADDRESS' });
   }, [dispatch]);
 
-  const [city, setCity] = useState(null);
-  const [stateName, setStateName] = useState(null)
-  const [street, setStreet] = useState(null)
-  const [zip, setZip] = useState(null)
+  const [markers, setMarkers] = useState([]);
+  const [center, setCenter] = useState({ lat: 40.748817, lng: -73.985428 });
+
+  useEffect(() => {
+    const convertAddressToLatLng = async () => {
+      const markersWithLatLng = await Promise.all(
+        addresses.map(async (address) => {
+          const geocodeResults = await getGeocode({ address: address.street + ", " + address.city + ", " + address.state + ", " + address.zip });
+          const { lat, lng } = await getLatLng(geocodeResults[0]);
+          return {
+            ...address,
+            lat,
+            lng
+          };
+        })
+      );
+      setMarkers(markersWithLatLng);
+
+      // Find the first marker with useraddress defined and update the center
+      const firstMarkerWithAddress = markersWithLatLng.find(marker => marker.useraddress);
+      if (firstMarkerWithAddress) {
+        setCenter({ lat: firstMarkerWithAddress.lat, lng: firstMarkerWithAddress.lng });
+      }
+    };
+
+    if (addresses.length > 0) {
+      convertAddressToLatLng();
+    }
+  }, [addresses]);
+
   const mapContainerStyle = {
     width: '100%',
     height: '100%'
   }
-  const center = {
-    lat:44.152959 , 
-    lng:-93.944348,
-  }
+
   const options = {
     styles: mapStyles,
     disableDefaultUI: true,
@@ -50,20 +71,27 @@ function UserPage() {
 
   if (loadError) return "Error loading maps"
   if (!isLoaded) return "Loading Maps"
+
   return (
     <div className="container">
-      <div className='MapContainer'><GoogleMap 
-      mapContainerStyle={mapContainerStyle}
-      zoom ={15}
-      center= {center}
-      options = {options}> </GoogleMap></div>
-      
-      <div> Test 
-      <div>{JSON.stringify(address)}</div> 
+      <h1>Active Sales</h1>
+      <div className='MapContainer'>
+        <GoogleMap 
+          mapContainerStyle={mapContainerStyle}
+          zoom={15}
+          center={center}
+          options={options}
+        >
+          {markers.map((marker) => (
+            <Marker
+              key={marker.id}
+              position={{ lat: marker.lat, lng: marker.lng }}
+            />
+          ))}
+        </GoogleMap>
       </div>
     </div>
   );
 }
 
-// this allows us to use <App /> in index.js
 export default UserPage;

e21a41f743adb92c30b5c973e9d1ecdff5ae1e15
Mitch Ludkey <mludk92@hotmail.com>
2023-05-30 15:15:14 -0500

    getting closer to wokring

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -19,7 +19,7 @@ function UserPage() {
   const addresses = useSelector((store) => store.address);
   
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: ,
+    googleMapsApiKey: '',
     libraries,
   });
 
@@ -30,10 +30,12 @@ function UserPage() {
   const [selectedMarker, setSelectedMarker] = useState(null);
   const [markers, setMarkers] = useState([]);
   const [center, setCenter] = useState({ lat: 40.748817, lng: -73.985428 });
+  const [openPopup, setOpenPopup] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [arrayOfItems, setArrayOfItems] = useState([]);
 
   useEffect(() => {
     if (isLoaded && addresses.length > 0) {
-      console.log(addresses, 'address array')
       setMarkers(addresses);
       const firstMarkerWithAddress = addresses.find((marker) => marker.useraddress);
       if (firstMarkerWithAddress) {
@@ -56,6 +58,26 @@ function UserPage() {
   if (loadError) return 'Error loading maps';
   if (!isLoaded) return 'Loading Maps';
 
+  const generateAndSendRoute = () => {
+    // Generate the route using the selectedItems array
+    // Use the Google Routing API
+
+    // Send the route via email
+    // Implement the email sending functionality
+  };
+
+  const removeFromArray = (id) => {
+    const updatedItems = selectedItems.filter((item) => item.id !== id);
+    setSelectedItems(updatedItems);
+    
+  
+    // Check if the selectedMarker is removed and set it to null
+    if (selectedMarker && selectedMarker.id === id) {
+      setSelectedMarker(null);
+    }
+  };
+  
+
   return (
     <div className="container">
       <h1>Active Sales</h1>
@@ -70,29 +92,60 @@ function UserPage() {
             <Marker
               key={marker.id}
               position={{ lat: marker.lat, lng: marker.lng }}
-              onClick={() => setSelectedMarker(marker)}
-              icon={{
-                url: marker.lat == center.lat ?  
-                '/images/house.png'
-                : '/images/ls.png',
-                scaledSize: marker.lat == center.lat ? new window.google.maps.Size(50, 50) 
-                :new window.google.maps.Size(60, 60),
+              onMouseOver={() => {
+                setSelectedMarker(marker);
+                setOpenPopup(true);
               }}
+              onClick={() => {
+                if (!selectedItems.some((item) => item.id === marker.id)) {
+                  const updatedItems = [...selectedItems, marker];
+                  setSelectedItems(updatedItems);
+                }
+              }}
+              icon={{
+                url: marker.lat === center.lat ? '/images/house.png' : '/images/ls.png',
+                scaledSize: new window.google.maps.Size(60, 60),
+                origin: new window.google.maps.Point(0, 0),
+                anchor: new window.google.maps.Point(18, 18),
+              }}
+              options={{
+                label: selectedItems.some((item) => item.id === marker.id) ? {
+                  text: 'Selected',
+                  color: 'black',
+                  fontWeight: 'bold',
+                  fontSize: '12px',
+                  background: 'rgba(0, 0, 255, 0.3)',
+                  borderRadius: '50%',
+                  padding: '5px',
+                } : null,
+              }}
+              
+              
+              
             />
           ))}
-          {selectedMarker && (
-            <InfoWindow 
+          {selectedMarker && openPopup && (
+            <InfoWindow
               position={{ lat: selectedMarker.lat, lng: selectedMarker.lng }}
-              onCloseClick={() => setSelectedMarker(null)}
+              onCloseClick={() => setOpenPopup(false)}
             >
-              <div className="popup" >
+              <div>
                 <h2>{selectedMarker.street}, {selectedMarker.city}</h2>
-                <p>Sales Dates: {selectedMarker.fromdate.slice(0,10)} - {selectedMarker.todate.slice(0,10)}</p>
+                <p>Sales Dates: {selectedMarker.fromdate} - {selectedMarker.todate}</p>
               </div>
             </InfoWindow>
           )}
         </GoogleMap>
       </div>
+      <div className='arrayof'>
+        {selectedItems.map((item) => (
+          <div key={item.id}>
+            <span className='listof'>{`   ${item.street}, ${item.city}, ${item.state}`}     </span>
+            <button onClick={() => removeFromArray(item.id)}>Remove</button>
+          </div>
+        ))}
+      </div>
+      <button onClick={generateAndSendRoute}>Generate Route and Send Email</button>
     </div>
   );
 }
